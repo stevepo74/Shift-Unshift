fd6f9822933334df89c631e09e2d1d631642363f
Steve P. Olson <36646456+stevepo74@users.noreply.github.com>
2021-05-13 09:08:53 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// exercise template
+
+//.shift
+//changes the length of an array
+
+const display = document.querySelector(".display");
+
+const movies = ["Titanic", "ET", "Matrix"];
+
+movies.shift();
+
+display.innerHTML = movies;
